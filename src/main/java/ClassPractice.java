4c712507ac7752f5ab3f563d95dafbc041cbb21e
shidurbaba <mpperis485@gmail.com>
2018-05-26 19:55:00 -0400

    First commit

---
+++ src/main/java/ClassPractice.java
@@ -0,0 +1,12 @@
+public class ClassPractice {
+    int var = 1;
+    int var1 = 2;
+    int var2, var3, var4, var5;
+
+    public void setVariable(int input2, int input3, int input4, int input5) {
+        this.var2 = input2;
+        this.var3 = input3;
+        this.var4 = input4;
+        this.var5 = input5;
+    }
+}
