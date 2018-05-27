3058a00854a64ea81cbd9fd410c99d038eb93f13
shidurbaba <mpperis485@gmail.com>
2018-05-26 20:08:45 -0400

    add changes

---
+++ src/main/java/ClassPracticeIO.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class ClassPracticeIO {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("C:\\Users\\moses\\Documents\\2018_Accenture_Training_Folder\\SimpleText.txt");
+
+        } catch (Exception e) {
+
+        }
+    }
+
+}
